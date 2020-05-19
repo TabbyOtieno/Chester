823029cc26df17c5318a76086ebd77643815d1a4
TabbyO <TabbyO@bitbucket.org>
2020-05-19 19:32:47 +0300

    Home Page

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Home Page</title>
+</head>
+
+<body>
+    <h1><center>Welcome to Chester's Cycling Outlet (CCO) Limited</center></h1>
+    
+    <div align="center">
+        <br><br><br>
+        <a href="login.php">Click here to log in</a>
+        <br><br>
+        <a href="signup.php">Don't have an account? Register Here</a>
+    </div>
+
+</body>
+
+</html>
+
